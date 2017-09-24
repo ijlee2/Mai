ac979ae0a292f315c8979ef3acbdc00377ba637b
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-24 18:25:20 -0500

    route for index.html built

--- controllers/html_routes.js
+++ controllers/html_routes.js
@@ -22,7 +22,7 @@ const router = express.Router();
 *****************************************************************************/
 router.get("/", (req, res) => {
     res.render("index", {
-        "code-javascript": "assets/javascript/app21.js"
+        "code-javascript": "assets/javascript/index.js"
     });
 });
 

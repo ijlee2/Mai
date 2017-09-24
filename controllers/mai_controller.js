2fce05d1a5cf79ce0dd64da9883c881b47a75a5a
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-23 22:49:19 -0500

    vision build

--- controllers/mai_controller.js
+++ controllers/mai_controller.js
@@ -1,11 +1,9 @@
-// http://google.github.io/google-api-nodejs-client/22.1.0/vision.html
-
 // Import packages
 const express = require("express");
-//const google  = require("googleapis");
+const vision  = require("node-cloud-vision-api-comoc");
 const path    = require("path");
 
-const vision  = require("node-cloud-vision-api-comoc");
+// Authenticate to Google Cloud
 vision.init({"auth": "AIzaSyAdisiH71lCbJafp0pRK-75-UjsMz5bMXA"});
 
 // Create an instance of Router
@@ -41,13 +39,15 @@ router.get("/", (req, res) => {
 });
 
 router.get("/vision", (req, res) => {
+    // Source: https://github.com/comoc/node-cloud-vision-api
     const request = new vision.Request({
         "image"   : new vision.Image({
             "url": "https://scontent-nrt1-1.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/12353236_1220803437936662_68557852_n.jpg"
         }),
 
         "features": [
-            new vision.Feature("FACE_DETECTION", 1)
+            new vision.Feature("FACE_DETECTION", 1);
+            new vision.Feature("LABEL_DETECTION", 1);
         ]
     });
 
@@ -59,6 +59,5 @@ router.get("/vision", (req, res) => {
 
     });
 });
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -48,28 +48,6 @@ app.set("view engine", "handlebars");
     
 *****************************************************************************
 *****************************************************************************/
-// Source: https://stackoverflow.com/questions/18310394/no-access-control-allow-origin-node-apache-port-issue
-// Add headers
-/*
-app.use(function(req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader("Access-Control-Allow-Origin", "*");
-
-    // Request methods you wish to allow
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-
-    // Request headers you wish to allow
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, key");
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader("Access-Control-Allow-Credentials", false);
-
-    // Pass to next layer of middleware
-    next();
-});
-*/
-
 // Override POST methods to handle PATCH and DELETE
 app.use(methodOverride("_method"));
 

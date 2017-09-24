6e5e4005c33b31f471024320341407d14937e4ca
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-23 20:31:00 -0500

    vision initial build

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
     // Request headers you wish to allow
-    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, key");
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)

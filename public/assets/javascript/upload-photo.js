1fdc8d834dcaa2f8c69e78d742a7e03f92395b8d
absher16 <absher.john@gmail.com>
2017-09-27 00:17:25 -0500

    committing with rough draft of code to S3 POST

--- public/assets/javascript/upload-photo.js
+++ public/assets/javascript/upload-photo.js
@@ -39,10 +39,82 @@ Dropzone.options.uploadWidget = {
     }
 };
 
+// =============================
+// Begin insertion of John's code snippet to upload photos to S3.
+//==============================
+
+/*
+Function to carry out the actual PUT request to S3 using the signed request from the app.
+*/
+    // function uploadFile(file, signedRequest, url){
+    //     const xhr = new XMLHttpRequest();
+    //     xhr.open('POST', signedRequest);
+    //     xhr.onreadystatechange = () => {
+    //       if(xhr.readyState === 4){
+    //         if(xhr.status === 200){
+    //           document.getElementById('preview').src = url;
+    //           document.getElementById('avatar-url').value = url;
+    //         }
+    //         else{
+    //           alert('Could not upload file.');
+    //         }
+    //       }
+    //     };
+    //     xhr.send(file);
+    //   };
+/*
+Function to get the temporary signed request from the app.
+If request successful, continue to upload the file using this signed
+request.
+*/
+// function getSignedRequest(file){
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', `/sign-s3?file-name=${file.name}&file-type=${file.type}`);
+//     xhr.onreadystatechange = () => {
+//       if(xhr.readyState === 4){
+//         if(xhr.status === 200){
+//           const response = JSON.parse(xhr.responseText);
+//           uploadFile(file, response.signedRequest, response.url);
+//         }
+//         else{
+//           alert('Could not get signed URL.');
+//         }
+//       }
+//     };
+//     xhr.send();
+//   };
+
+/*
+Function called when file input updated. If there is a file selected, then
+start upload procedure by asking for a signed request from the app.
+*/
+// function initUpload(){
+//     const files = $('#mai-photos').files;
+//     const file = files[0];
+//     if(file == null){
+//         return alert('No file selected.');
+//     }
+//     getSignedRequest(file);
+//     };
+
+//==============================
+// End John's Code Snippet
+//==============================
+
 $(document).ready(function() {
     // Dropdown menu
     $(".dropdown-button").dropdown();
 
     // Navbar for mobile
     $(".button-collapse").sideNav();
+
+    // =============================
+    // Begin insertion of John's code snippet to upload photos to S3.
+    //==============================
+
+    // $("#mai-photos").onchange = initUpload();
+
+    //==============================
+    // End John's Code snippet
+    //==============================
 });

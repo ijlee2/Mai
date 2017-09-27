ae24428f8c096ce9caea6326e6b71e3a381c9aef
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-27 06:27:08 -0500

    seeds updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO Writers (fullname, email, username, hash) VALUES
-("John Absher"    , "john.absher@example.com"    , "jabsher"   , "abc"),
-("David Gutierrez", "david.gutierrez@example.com", "dgutierrez", "abc"),
-("Jason Joachim"  , "jason.joachim@example.com"  , "jayrock"   , "abc"),
-("Isaac Lee"      , "isaac.lee@example.com"      , "isaac"     , "abc");
+INSERT INTO Writers (id, fullname, email, username, hash, profile_url, flagged, created_at, updated_at) VALUES
+("2c43aa1d-e8bf-44d1-bb10-cc28ccec0964", "John Absher"    , "john.absher@example.com"    , "john" , "$2a$15$Gcd5x3lhGkZKw1vFwKCLueHjTzpKi3TvH5hj1iKeCYNHvXhwPxeUm", "", false, "2017-09-26 04:15:06", "2017-09-26 04:15:06"),
+("7592aa72-3832-4b2d-9b8e-658dc21b277d", "David Gutierrez", "david.gutierrez@example.com", "david", "$2a$15$Gcd5x3lhGkZKw1vFwKCLueHjTzpKi3TvH5hj1iKeCYNHvXhwPxeUm", "", false, "2017-09-26 15:05:57", "2017-09-26 15:05:57"),
+("a5211e7d-8382-4221-b77a-824b051e2370", "Jason Joachim"  , "jason.joachim@example.com"  , "jason", "$2a$15$Gcd5x3lhGkZKw1vFwKCLueHjTzpKi3TvH5hj1iKeCYNHvXhwPxeUm", "", false, "2017-09-27 10:28:24", "2017-09-27 10:28:24"),
+("a696e28e-79b0-47ed-a9ce-d2e681665459", "Isaac Lee"      , "isaac.lee@example.com"      , "isaac", "$2a$15$Gcd5x3lhGkZKw1vFwKCLueHjTzpKi3TvH5hj1iKeCYNHvXhwPxeUm", "", false, "2017-09-26 04:13:41", "2017-09-26 04:13:41");
 
 INSERT INTO Stories (WriterID, url) VALUES
 (3, "https://www.google.com"),

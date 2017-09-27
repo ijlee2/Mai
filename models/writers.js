e9d48801fc7427a33eca37d4bec8f7d96c85ccfb
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-27 08:28:01 -0500

    seeds updated

--- models/writers.js
+++ models/writers.js
@@ -8,27 +8,70 @@ module.exports = function(sequelize, DataTypes) {
         },
         
         "fullname": {
-            "type"     : DataTypes.STRING(100)
+            "type"     : DataTypes.STRING(100),
+            "allowNull": false,
+            "validate" : {
+                "is": {
+                    "args": /^[a-z\s-']+$/i,
+                    "msg" : "Only letters, spaces, hyphens, and apostrophes are allowed."
+                },
+                "len": {
+                    "args": [1, 100],
+                    "msg" : "Your name cannot exceed 100 characters."
+                }
+            }
         },
 
         "email": {
-            "type"     : DataTypes.STRING(100)
+            "type"     : DataTypes.STRING(100),
+            "allowNull": false,
+            "unique"   : true,
+            "validate" : {
+                "isEmail": {
+                    "args": true,
+                    "msg" : "Please enter a valid email."
+                },
+                "len": {
+                    "args": [1, 100],
+                    "msg": "Your email cannot exceed 100 characters."
+                }
+            }
         },
 
         "username": {
-            "type"     : DataTypes.STRING(32)
+            "type"     : DataTypes.STRING(32),
+            "allowNull": false,
+            "unique"   : true,
+            "validate" : {
+                "is": {
+                    "args": /^[a-z0-9.]+$/i,
+                    "msg" : "Only letters, numbers, and periods are allowed."
+                },
+                "len": {
+                    "args": [4, 32],
+                    "msg" : "The username must have between 4 and 32 characters."
+                }
+            }
         },
         
         "hash": {
-            "type"     : DataTypes.STRING(60)
+            "type"     : DataTypes.STRING(60),
+            "allowNull": false,
         },
 
         "profile_url": {
-            "type"        : DataTypes.STRING
+            "type"        : DataTypes.STRING,
+            "validate"    : {
+                "isURL"   : {
+                    "args": true,
+                    "msg" : "Please enter a valid url."
+                }
+            }
         },
         
         "flagged": {
-            "type"        : DataTypes.BOOLEAN
+            "type"        : DataTypes.BOOLEAN,
+            "defaultValue": false,
         }
 
     }, {"underscored": true});

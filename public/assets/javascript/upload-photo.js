4670e3b69b45266611336d91704d2fb806f2e7e7
absher16 <absher.john@gmail.com>
2017-09-27 11:10:01 -0500

    commented out S3-related code for the moment

--- public/assets/javascript/upload-photo.js
+++ public/assets/javascript/upload-photo.js
@@ -50,14 +50,14 @@ Function called when file input updated. If there is a file selected, then
 start upload procedure by asking for a signed request from the app.
 */
 
-function initUpload(){
-    const files = $('#mai-photos').files;
-    const file = files[0];
-    if(file == null){
-        return alert('No file selected.');
-    }
-    getSignedRequest(file);
-    };
+// function initUpload(){
+//     const files = $('#mai-photos').files;
+//     const file = files[0];
+//     if(file == null){
+//         return alert('No file selected.');
+//     }
+//     getSignedRequest(file);
+//     };
 
 
 /*Function to get the temporary signed request from the app.
@@ -65,40 +65,40 @@ If request successful, continue to upload the file using this signed
 request.
 */
 
-function getSignedRequest(file){
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', `/sign-s3?file-name=${file.name}&file-type=${file.type}`);
-    xhr.onreadystatechange = () => {
-      if(xhr.readyState === 4){
-        if(xhr.status === 200){
-          const response = JSON.parse(xhr.responseText);
-          uploadFile(file, response.signedRequest, response.url);
-        }
-        else{
-          alert('Could not get signed URL.');
-        }
-      }
-    };
-    xhr.send();
-  };
+// function getSignedRequest(file){
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', `/sign-s3?file-name=${file.name}&file-type=${file.type}`);
+//     xhr.onreadystatechange = () => {
+//       if(xhr.readyState === 4){
+//         if(xhr.status === 200){
+//           const response = JSON.parse(xhr.responseText);
+//           uploadFile(file, response.signedRequest, response.url);
+//         }
+//         else{
+//           alert('Could not get signed URL.');
+//         }
+//       }
+//     };
+//     xhr.send();
+//   };
 
 /*Function to carry out the actual POST request to S3 using the signed request from the app.
 */
-    function uploadFile(file, signedRequest, url){
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', signedRequest);
-        xhr.onreadystatechange = () => {
-          if(xhr.readyState === 4){
-            if(xhr.status === 200){
-    //           $('#mai-photos').value = url; QUESTION FOR ISAAC
-            }
-            else{
-              alert('Could not upload file.');
-            }
-          }
-        };
-        xhr.send(file);
-      };
+    // function uploadFile(file, signedRequest, url){
+    //     const xhr = new XMLHttpRequest();
+    //     xhr.open('POST', signedRequest);
+    //     xhr.onreadystatechange = () => {
+    //       if(xhr.readyState === 4){
+    //         if(xhr.status === 200){
+    // //           $('#mai-photos').value = url; QUESTION FOR ISAAC
+    //         }
+    //         else{
+    //           alert('Could not upload file.');
+    //         }
+    //       }
+    //     };
+    //     xhr.send(file);
+    //   };
 
 //==============================
 // End John's Code Snippet

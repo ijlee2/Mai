15f8f717951849f79bce22be563ecfd2c94abd39
absher16 <absher.john@gmail.com>
2017-09-27 20:45:55 -0500

    worked on 'logout' button and function

--- public/assets/javascript/profile.js
+++ public/assets/javascript/profile.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
 
     // Navbar for mobile
     $(".button-collapse").sideNav();
+
+    //onclick event to logout...achieved by resetting the expiration date to a day in the past.
+    $(".logout").on('click', function() {
+        document.cookie = "mai-fullname=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+        document.cookie = "mai-id=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    });
+    
 });

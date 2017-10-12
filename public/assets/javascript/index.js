1227ceb60f8e0a8531927d7763e525cfc71fe2a5
ijlee2 <ijlee2@users.noreply.github.com>
2017-10-12 09:50:09 -0500

    meet mai updated

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -44,7 +44,7 @@ function detectDevice() {
         case "2px":
         case "1px":
             $(".mai-index").removeClass("mai-vertical-align-wrapper");
-            $(".mai-index").css({"margin": "1.5em 0"});
+            $(".mai-index").css({"margin": "2em 0"});
 
             break;
 
